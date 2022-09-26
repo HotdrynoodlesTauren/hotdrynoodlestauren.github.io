a510a6ea5bcec007b798c2bdc01ca4367e2c02ae
Yi Zhou <92965215+HotdrynoodlesTauren@users.noreply.github.com>
2022-09-26 14:42:43 -0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,12 +1,14 @@
+// I don't think this is an error as the tutorial told me to just put this exact same line here and it worked
+// I suppose it's used in html files
 const swup = new Swup()
 
-var pic = document.querySelector('img.cat');
+var pic = document.querySelector('img.cat')
 document.addEventListener('mousemove', function(e){
-    // console.log(1)
-    var x = e.pageX;
-    var y = e.pageY;
-    x = x + 4
-    // y = y + 1
-    pic.style.left = x + 'px';
-    pic.style.top = y + 'px';
-});
+	// console.log(1)
+	var x = e.pageX
+	var y = e.pageY
+	x = x + 4
+	// y = y + 1
+	pic.style.left = x + 'px'
+	pic.style.top = y + 'px'
+})
